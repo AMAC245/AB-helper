126393d511bd3f2a8ba7b570b9f5eb9fe1ae47af
Andrew McGillivray <ap.mcgillivray@gmail.com>
2017-11-24 11:54:25 +1100

    Update descriptives.js

--- descriptives.js
+++ descriptives.js
@@ -3,11 +3,23 @@
 
 const descriptives = sample => {
     const sum = sample.reduce((prev, curr) => curr += prev);
+    
     return {
         population: sample.length,
+        
         mean() {
             return sum / this.population
-        }
+        },
+        
+        sd() {
+            const calculateIndex = sample.map(index => {
+                return (index - this.mean()) ** 2
+            });
+            return indexIterator
+                .reduce((prev, curr) => curr += prev) // ::: extract to higher-order (duplicating mean logic) :::
+                / this.population
+        }  
     }
+    
 }
 export default descriptives

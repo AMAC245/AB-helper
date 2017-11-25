4df412efa33baa96bea6816ba80b06133c9ed984
Andrew McGillivray <ap.mcgillivray@gmail.com>
2017-11-25 20:33:15 +1100

    Update descriptives.js

--- descriptives.js
+++ descriptives.js
@@ -22,4 +22,54 @@ const descriptives = sample => {
     }
     
 }
+
+// :::higher-orders::: 
+const sum = sample => {
+    return sample.reduce((prev, curr) => curr += prev);
+};
+
+const mean = sample => {
+    return sum(sample) / sample.length
+}
+
+
+// ::facotry function that returns the descriptive stats for a given sample::
+
+const descriptives = sample => {
+    return {
+        population: sample.length,
+        mean: mean(sample),
+        sd: sd(sample)
+    }
+}
+
+// ::SAMPLE STANDARD DEVIATION::
+const sd = sample => {
+    
+    const calculateIndex = sample.map(index => {
+        return (index - mean(sample)) ** 2
+    });
+    
+    const sampleVariance = (sum(calculateIndex)) / (sample.length - 1)
+    
+    return Math.sqrt(sampleVariance)
+}
+
+// determines whether the two sample variances are equal or not
+const f_statistic = (control, treatment) => {
+    const crit_value = 2.25; 
+  
+    const f_stat = 
+          return sd(control) > sd(treatment)
+            ? sd(control) / sd(treatment)
+            : sd(treatment) / sd(control)
+    
+    return f_stat > crit_value 
+            ? 'unequal' 
+            : 'equal'
+}
+
+if (f_statistic === 'unequal' ) {
+    (descriptives(control).mean - descriptives(treatment).mean)
+}    
 export default descriptives

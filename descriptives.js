a924656c45ad2e095a6048fe681b1f9d8fd00797
Andrew McGillivray <ap.mcgillivray@gmail.com>
2017-11-27 13:07:25 +1100

    Update descriptives.js

--- descriptives.js
+++ descriptives.js
@@ -1,28 +1,3 @@
-
-// factory function that collects the descriptive stats of each group
-
-const descriptives = sample => {
-    const sum = sample.reduce((prev, curr) => curr += prev);
-    
-    return {
-        population: sample.length,
-        
-        mean() {
-            return sum / this.population
-        },
-        
-        sd() {
-            const calculateIndex = sample.map(index => {
-                return (index - this.mean()) ** 2
-            });
-            return indexIterator
-                .reduce((prev, curr) => curr += prev) // ::: extract to higher-order (duplicating mean logic) :::
-                / this.population
-        }  
-    }
-    
-}
-
 // :::higher-orders::: 
 const sum = sample => {
     return sample.reduce((prev, curr) => curr += prev);

b7a6f9c5e0eb104a8258a5f176ac2c3f93050d13
Andrew McGillivray <ap.mcgillivray@gmail.com>
2017-12-04 22:26:19 +1100

    Update unequal_t_test.js

--- unequal_t_test.js
+++ unequal_t_test.js
@@ -2,17 +2,17 @@ const { descriptives, variance, x, y } = require('./descriptives');
 const { sum, mean } = require('./utils/higher-orders');
 const t_table = require('./t_test');
 
-const unequal_t_test = (x, y) => {
+const unequal_t_test = (ctrl, trtmt) => {
     const sd_util = sample => {
         return variance(sample) / descriptives(sample).population
     }
 
-    const t_stat = (mean(x) - mean(y)) / 
-                   (Math.sqrt(sd_util(x) + sd_util(y)))
+    const t_stat = (mean(ctrl) - mean(trtmt)) / 
+                   (Math.sqrt(sd_util(ctrl) + sd_util(trtmt)))
 
-    const _df = Math.round((sd_util(x) + sd_util(y)) ** 2 /
-                (((sd_util(x) ** 2) / descriptives(x).df) +
-                ((sd_util(y) ** 2) / descriptives(y).df)))
+    const _df = Math.round((sd_util(ctrl) + sd_util(trtmt)) ** 2 /
+                (((sd_util(ctrl) ** 2) / descriptives(ctrl).df) +
+                ((sd_util(trtmt) ** 2) / descriptives(trtmt).df)))
     
     return t_stat < t_table[_df]
         ? false 
@@ -22,4 +22,4 @@ const unequal_t_test = (x, y) => {
 const compare = (control, treatment) => ({
     significance: unequal_t_test(control, treatment)
 })
-
+console.log(compare(x,y))
